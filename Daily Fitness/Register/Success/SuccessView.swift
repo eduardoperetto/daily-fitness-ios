2551357bd1f278ba94a99dc440e2f6f8b8009560
eduardoperetto <erperetto@gmail.com>
2023-02-23 22:28:55 -0300

    feat: success view

--- Daily Fitness/Register/Success/SuccessView.swift
+++ Daily Fitness/Register/Success/SuccessView.swift
@@ -5,4 +5,91 @@
 //  Created by Eduardo Raupp Peretto on 22/02/23.
 //
 
-import Foundation
+import UIKit
+
+final class SuccessView: UIView {
+    // MARK: - Properties
+
+    private let openHome: () -> Void
+    @objc private func _openHome() { openHome() }
+
+    // MARK: - Inner Views
+
+    private let iconView: UIImageView = {
+        let image = UIImage(systemName: "checkmark")?.withTintColor(.black, renderingMode: .alwaysOriginal)
+        let imageView = UIImageView(image: image)
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        return imageView
+    }()
+
+    private let successLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Meal saved succesfully"
+        label.font = .systemFont(ofSize: 24)
+        label.textColor = .black
+        label.textAlignment = .center
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+
+    private let returnButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("Return home", for: .normal)
+        button.backgroundColor = .systemGray5
+        button.setTitleColor(.black, for: .normal)
+        button.layer.cornerRadius = 15
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+
+    // MARK: - Init
+
+    init(openHome: @escaping () -> Void) {
+        self.openHome = openHome
+        super.init(frame: .zero)
+        setupViews()
+    }
+
+    @available(*, unavailable)
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    // MARK: - Methods
+
+    private func setupViews() {
+        backgroundColor = .white
+
+        addSubview(iconView)
+        addSubview(successLabel)
+        addSubview(returnButton)
+        returnButton.addTarget(self, action: #selector(_openHome), for: .touchDown)
+
+        configureConstraints()
+    }
+
+    private func configureConstraints() {
+        let screenWidth: CGFloat = 260
+        let buttonHeight: CGFloat = 70
+
+        NSLayoutConstraint.activate([
+            // Title label
+            iconView.centerXAnchor.constraint(equalTo: centerXAnchor),
+            iconView.centerYAnchor.constraint(equalTo: topAnchor, constant: 300),
+            iconView.widthAnchor.constraint(equalToConstant: 100),
+            iconView.heightAnchor.constraint(equalToConstant: 100),
+
+            // Logs button
+            successLabel.centerXAnchor.constraint(equalTo: centerXAnchor),
+            successLabel.centerYAnchor.constraint(equalTo: iconView.bottomAnchor, constant: 100),
+            successLabel.widthAnchor.constraint(equalToConstant: screenWidth),
+            successLabel.heightAnchor.constraint(equalToConstant: buttonHeight),
+
+            // Register button
+            returnButton.centerXAnchor.constraint(equalTo: centerXAnchor),
+            returnButton.centerYAnchor.constraint(equalTo: bottomAnchor, constant: -150),
+            returnButton.widthAnchor.constraint(equalToConstant: screenWidth),
+            returnButton.heightAnchor.constraint(equalToConstant: buttonHeight)
+        ])
+    }
+}
--- Daily Fitness/Register/Success/SuccessViewController.swift
+++ Daily Fitness/Register/Success/SuccessViewController.swift
@@ -5,4 +5,42 @@
 //  Created by Eduardo Raupp Peretto on 22/02/23.
 //
 
-import Foundation
+import UIKit
+
+final class SuccessViewController: UIViewController {
+    // MARK: - Properties
+
+    private lazy var rootView = SuccessView(
+        openHome: { [weak self] in self?.openHome() }
+    )
+
+    // MARK: - Init
+
+    init() {
+        super.init(nibName: nil, bundle: nil)
+    }
+
+    @available(*, unavailable)
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    // MARK: - LifeCycle
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view = rootView
+        setupNavigation()
+    }
+
+    // MARK: - Methods
+
+    private func setupNavigation() {
+        navigationController?.navigationBar.backgroundColor = .white
+    }
+
+    private func openHome() {
+        let homeController = HomeViewController()
+        navigationController?.setViewControllers([homeController], animated: true)
+    }
+}
